c42703c998c9d26b66f8bf88392f60e1221bbdb2
brunoredes <bruno.donatelli@etec.sp.gov.br>
2020-04-12 11:28:51 -0300

    Like challenge completed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const { uuid, isUuid } = require("uuidv4");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,88 @@ app.use(cors());
 
 const repositories = [];
 
+function validateRepositoryId(request, response, next) {
+  const { id } = request.params;
+
+  if(!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid repository ID' });
+  }
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+   const repo = {
+     id: uuid(), 
+     title, 
+     url, 
+     techs,
+     likes: 0 
+    };
+
+   repositories.push(repo);
+
+   return response.json(repo);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateRepositoryId, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const findRepo = repositories.findIndex(r => r.id === id);
+
+  if(findRepo === -1) {
+    return response.status(400).json({ error: 'Repository does not exists' });
+  }
+
+  const repo = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[findRepo].likes,
+  }
+
+  repositories[findRepo] = repo;
+
+  return response.json(repo);
+
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateRepositoryId, (request, response) => {
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(r => r.id === id);
+
+  if(repoIndex >= 0) {
+    repositories.splice(repoIndex, 1);
+  }
+  else {
+    return response.status(400).json({ error: 'Repository does not exists' })
+  }
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateRepositoryId, (request, response) => {
+  const { id } = request.params;
+
+  const findRepo = repositories.findIndex(r => r.id === id);
+
+  if(findRepo === -1) {
+    return response.status(400).json({ error: 'Repository does not exists' });
+  }
+ 
+  repositories[findRepo].likes += 1;
+
+  return response.json(repositories[findRepo]);
+
 });
 
 module.exports = app;
